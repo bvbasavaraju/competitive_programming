5d26c18bde5f1067d3f0a908d7da2b26f997bbed
batbug <bv.basavaraju@gmail.com>
2020-02-13 00:39:33 +0530

    + solved the unsolved problem #16 + modified the gitignore to not to consider .vscode and its contents for checkin

--- leetcode/problems/leetcode_problem_num_16.cpp
+++ leetcode/problems/leetcode_problem_num_16.cpp
@@ -15,47 +15,103 @@ using namespace std;
 class Solution
 {
 private:
-    vector<vector<int>> groups;
+  // vector<vector<int>> groups;
 
-    void FillInPossibleCombination(vector<int> &nums, int l, int i, vector<int> set)
+  // bool FillInPossibleCombination(vector<int> &nums, int l, int i, vector<int> set)
+  // {
+  //   if ((set.size() > 2) || (i >= l))
+  //   {
+  //     if(set.size() > 2)
+  //     {
+  //       groups.push_back(set);
+  //     }
+
+  //     // No need to call the recursive function with incremented value of i again!
+  //     return false;
+  //   }
+
+  //   set.push_back(nums[i]);
+  //   for(int j = i; j < l; j++)
+  //   {
+  //     if(!FillInPossibleCombination(nums, l, j + 1, set))
+  //     {
+  //       break;
+  //     }
+  //   }
+  //   // {
+  //   //   //Only if the function has to be called again with incremented value of "i", only then call the function again!
+  //   //   FillInPossibleCombination(nums, l, i + 2, set);
+  //   // }
+
+  //   return true;
+  // }
+
+  int ans;
+  int prevSmallDiff;
+  int tar;
+  bool SumOfThreeNumbersInSet(vector<int> &nums, int l, int pos, int numsAdded, int currSum)
+  {
+    if((numsAdded == 3) || (pos >= l))
     {
-        if (set.size() > 2)
+      if(numsAdded == 3)
+      {
+        //If the currSum is closest then update the diff of sum and target and update the answer
+        if(prevSmallDiff > abs(currSum - tar))
         {
-            groups.push_back(set);
-            return;
+          prevSmallDiff = abs(currSum - tar);
+          ans = currSum;
         }
+      }
 
-        set.push_back(nums[i]);
-        FillInPossibleCombination(nums, l, i + 1, set);
-        FillInPossibleCombination(nums, l, i + 2, set);
+      return false;
     }
+
+    currSum += nums[pos];
+    numsAdded++;
+    for(int i = pos; i < l; i++)
+    {
+      if(!SumOfThreeNumbersInSet(nums, l, i+1, numsAdded, currSum))
+      {
+        break;
+      }
+    }
+
+    return true;
+  }
 
 public:
-    int threeSumClosest(vector<int> &nums, int target)
+  int threeSumClosest(vector<int> &nums, int target)
+  {
+    ans = 1e9;
+    prevSmallDiff = 1e9;
+    tar = target;
+    int l = nums.size();
+
+    // As three numbers need to be added, excluding last 2 number loop for all other numbers.
+    for(int i = 0; i < l - 2; i++)
     {
-        int l = nums.size();
+      // vector<int> set;
+      // FillInPossibleCombination(nums, l, i, set);
 
-        vector<int> set;
-        FillInPossibleCombination(nums, l, 0, set);
-        for (int i = 0; i < groups.size(); i++)
-        {
-            vector<int> g = groups[i];
-            for (auto a : g)
-            {
-                printf("%d, ", a);
-            }
-            printf("\n");
-        }
-
-        return 0;
+      SumOfThreeNumbersInSet(nums, l, i, 0, 0);
     }
+
+    return (ans == 1e9) ? 0 : ans;
+  }
 };
 
 int main()
 {
-    vector<int> data = {-1, 2, 1, 4};
+    vector<int> data = {0, 1, 2};
+    int target = 3;
+
+    // vector<int> data = {-1, 2, 1, 4};
+    // int target = 1;
+
+    // vector<int> data = {1,2,4,8,16,32,64,128};
+    // int target = 82;
 
     Solution s;
-    s.threeSumClosest(data, 1);
+    s.threeSumClosest(data, target);
     return 0;
 }
