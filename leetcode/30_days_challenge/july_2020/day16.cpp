3d315c686d71e2fb1ae2a5cdeed8e3383a43c776
batbug <bv.basavaraju@gmail.com>
2020-07-17 07:34:28 +0530

    day 16 challenge

---
+++ leetcode/30_days_challenge/july_2020/day16.cpp
@@ -0,0 +1,65 @@
+/****************************************************
+Date: July 15th
+
+link: https://leetcode.com/explore/featured/card/july-leetcoding-challenge/544/week-1-july-1st-july-7th/3377/
+****************************************************/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <limits.h>
+
+using namespace std;
+
+/*
+  Q: Pow(x, n)
+
+  Implement pow(x, n), which calculates x raised to the power n (xn).
+
+  Example 1:
+    Input: 2.00000, 10
+    Output: 1024.00000
+
+  Example 2:
+    Input: 2.10000, 3
+    Output: 9.26100
+
+  Example 3:
+    Input: 2.00000, -2
+    Output: 0.25000
+
+  Explanation: 2-2 = 1/22 = 1/4 = 0.25
+  
+  Note:
+    -100.0 < x < 100.0
+    n is a 32-bit signed integer, within the range [−231, 231 − 1]
+*/
+class Solution 
+{
+public:
+  double myPow(double x, int n) 
+  {
+    if(x == 1 || n == 0)
+    {
+      return 1.0;
+    }
+    
+    bool neg = n < 0 ? true : false;
+    double ans = 1.0;
+    
+    int64_t n_ = n;
+    n_ = abs(n_);
+    
+    ans = pow(x, n_);
+    
+    return neg ? (1.0 / ans) : ans;  
+  }
+};
