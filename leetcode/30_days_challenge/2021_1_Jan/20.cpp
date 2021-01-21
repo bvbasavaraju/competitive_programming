62e91778a6dfc8a53682a11f466b1b2fb56c112d
batbug <bv.basavaraju@gmail.com>
2021-01-21 22:42:53 +0530

    + daily challenge

---
+++ leetcode/30_days_challenge/2021_1_Jan/20.cpp
@@ -0,0 +1,138 @@
+/****************************************************
+Date: Jan 20th
+
+link: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3606/
+****************************************************/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <limits.h>
+
+using namespace std;
+
+/*
+  Q: Valid Parentheses
+
+  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+  An input string is valid if:
+  + Open brackets must be closed by the same type of brackets.
+  + Open brackets must be closed in the correct order.
+
+  Example 1:
+    Input: s = "()"
+    Output: true
+  
+  Example 2:
+    Input: s = "()[]{}"
+    Output: true
+
+  Example 3:
+    Input: s = "(]"
+    Output: false
+
+  Example 4:
+    Input: s = "([)]"
+    Output: false
+
+  Example 5:
+    Input: s = "{[]}"
+    Output: true
+
+  Constraints:
+    1 <= s.length <= 104
+    s consists of parentheses only '()[]{}'.
+  
+  Hide Hint #1  
+    An interesting property about a valid parenthesis expression is that a sub-expression of a valid expression should also be a valid expression. (Not every sub-expression) e.g.
+      { { } [ ] [ [ [ ] ] ] } is VALID expression
+                [ [ [ ] ] ]    is VALID sub-expression
+        { } [ ]                is VALID sub-expression
+      Can we exploit this recursive structure somehow?
+  
+  Hide Hint #2  
+    What if whenever we encounter a matching pair of parenthesis in the expression, we simply remove it from the expression? This would keep on shortening the expression. e.g.
+      { { ( { } ) } }
+            |_|
+
+      { { (      ) } }
+          |______|
+
+      { {          } }
+        |__________|
+
+      {                }
+      |________________|
+      VALID EXPRESSION!
+
+  Hide Hint #3  
+    The stack data structure can come in handy here in representing this recursive structure of the problem. 
+    We can't really process this from the inside out because we don't have an idea about the overall structure. 
+    But, the stack can help us process this recursively i.e. from outside to inwards.
+*/
+
+class Solution 
+{  
+public:
+  bool isValid(string s) 
+  {
+    stack<char> container;
+    for(char ch : s)
+    {
+      bool does_match = false;
+      switch(ch)
+      {
+        case '(':
+        case '[':
+        case '{':
+            container.push(ch);
+            continue;
+          break;
+          
+        case ')':
+            if(!container.empty() && container.top() == '(')
+            {
+              does_match = true;
+            }
+          break;
+
+        case ']':
+            if(!container.empty() && container.top() == '[')
+            {
+              does_match = true;
+            }
+          break;
+          
+        case '}':
+            if(!container.empty() && container.top() == '{')
+            {
+              does_match = true;
+            }
+          break;
+          
+        default:
+          continue;
+      }
+      
+      if(does_match)
+      {
+        container.pop();
+      }
+      else
+      {
+        return false;
+      }
+    }
+    
+    return container.empty();
+  }
+};
