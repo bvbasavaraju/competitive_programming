33d5cbe5374b92dc6df622d6653a12b3341175a0
batbug <bv.basavaraju@gmail.com>
2021-01-26 10:32:42 +0530

    + top interview questions - string 1

---
+++ leetcode/problems/top_interview_questions_strings_1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+  Q: Reverse String
+
+  Write a function that reverses a string. The input string is given as an array of characters char[].
+  Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+  You may assume all the characters consist of printable ascii characters.
+
+  Example 1:
+    Input: ["h","e","l","l","o"]
+    Output: ["o","l","l","e","h"]
+
+  Example 2:
+    Input: ["H","a","n","n","a","h"]
+    Output: ["h","a","n","n","a","H"]
+*/
+
+class Solution 
+{
+public:
+  void reverseString(vector<char>& s) 
+  {
+    int l = s.size();
+    for(int i = 0; i < l/2; ++i)
+    {
+      char ch = s[i];
+      s[i] = s[l-i-1];
+      s[l-i-1] = ch;
+    }
+  }
+};
+
+/*
+  Hints:
+  Hide Hint #1  
+    The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+*/
