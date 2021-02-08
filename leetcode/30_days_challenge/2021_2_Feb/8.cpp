821a4cf1806b20a0918271531824bd16d2f5bff2
batbug <bv.basavaraju@gmail.com>
2021-02-08 22:18:51 +0530

    improvement in the code

--- leetcode/30_days_challenge/2021_2_Feb/8.cpp
+++ leetcode/30_days_challenge/2021_2_Feb/8.cpp
@@ -48,69 +48,57 @@ using namespace std;
     For a clean implementation, check out Google's guava library source code.
 */
 
-/*
- * Below is the interface for Iterator, which is already defined for you.
- * **DO NOT** modify the interface for Iterator.
- *
- *  class Iterator {
- *		struct Data;
- * 		Data* data;
- *		Iterator(const vector<int>& nums);
- * 		Iterator(const Iterator& iter);
- *
- * 		// Returns the next element in the iteration.
- *		int next();
- *
- *		// Returns true if the iteration has more elements.
- *		bool hasNext() const;
- *	};
- */
+class Iterator 
+{
+  struct Data;
+  Data* data;
+  Iterator(const vector<int>& nums);
+  Iterator(const Iterator& iter);
+
+  // Returns the next element in the iteration.
+  int next();
+
+  // Returns true if the iteration has more elements.
+  bool hasNext() const;
+};
 
 class PeekingIterator : public Iterator 
 {
 private:
-  vector<int> container;
-  vector<int>::iterator ptr = container.end();
+  int val;
+  bool has_next;
   
 public:
-  PeekingIterator(const vector<int>& nums) : Iterator(nums) 
+  PeekingIterator(const vector<int>& nums) : Iterator(nums), val(0), has_next(false)
   {
-      // Initialize any member here.
-      // **DO NOT** save a copy of nums and manipulate it directly.
-      // You should only use the Iterator interface methods.
-    
-    int l = nums.size();
-    for(int i = 0; i < l; ++i)
+    has_next = Iterator::hasNext();
+    if(has_next)
     {
-      container.push_back(nums[i]);
+      val = Iterator::next();
     }
-    
-    ptr = container.begin();
   }
   
-    // Returns the next element in the iteration without advancing the iterator.
   int peek() 
   {
-    if(hasNext())
-    {
-      return *ptr;
-    }
-    
-    return 0;
+    return val;
   }
   
   // hasNext() and next() should behave the same as in the Iterator interface.
   // Override them if needed.
   int next() 
   {
-    int ans = *ptr;
-    ptr++;
+    int ans = val;
+    has_next = Iterator::hasNext();
+    if(has_next)
+    {
+      val = Iterator::next();
+    }
     
     return ans;
   }
   
   bool hasNext() const 
   {
-    return ptr != container.end();    
+    return has_next;
   }
 };
