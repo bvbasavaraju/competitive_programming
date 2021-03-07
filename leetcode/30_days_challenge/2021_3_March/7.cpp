09cf4ea654d4bcd4afde8faa6c30cf8b69349bac
batbug <bv.basavaraju@gmail.com>
2021-03-07 18:35:57 +0530

    + daily challenge better solution!!

--- leetcode/30_days_challenge/2021_3_March/7.cpp
+++ leetcode/30_days_challenge/2021_3_March/7.cpp
@@ -46,85 +46,72 @@ using namespace std;
     Please do not use the built-in HashMap library.
 */
 
-//Too much memroy! Optimize it!!
-
 class MyHashMap 
 {
 private:
   
-  struct Data_t
-  {
-    int val;
-    Data_t* next;
-    Data_t* prev;
-    
-    Data_t(int val_) : val(val_), next(nullptr), prev(nullptr) {};
-  };
-  
-  Data_t* data;
-  vector<Data_t*> keys;
+  vector<vector<pair<int, int>>> data;
   
 public:
     /** Initialize your data structure here. */
     MyHashMap() 
-      : data(nullptr)
     {
-      keys.resize(1000001, nullptr);
+      data.resize(10001);
     }
     
     /** value will always be non-negative. */
     void put(int key, int value) 
     {
-      Data_t* data_ =  new Data_t(value);
+      int hash = key % 10000;
+      vector<pair<int, int>>& vals = data[hash];
       
-      if(data == nullptr)
+      bool found = false;
+      if(!vals.empty())
       {
-        data = data_;
-      }
-      else
-      {
-        data->next = data_;
-        data_->prev = data;
-        data = data_;
+        for(auto& p : vals)
+        {
+          if(p.first == key)
+          {
+            p.second = value;
+            found = true;
+          }
+        }
       }
       
-      keys[key] = data_;
+      if(!found)
+      {
+        data[hash].push_back({key, value});
+      }
     }
     
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     int get(int key) 
     {      
-      return (keys[key] != nullptr) ? keys[key]->val : -1;
+      int hash = key % 10000;
+      vector<pair<int, int>>& vals = data[hash];
+      for(auto p : vals)
+      {
+        if(p.first == key)
+        {
+          return p.second;
+        }
+      }
+      
+      return -1;
     }
     
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     void remove(int key) 
     {
-      Data_t* data_ = keys[key];
-      if(data_ != nullptr)
+      int hash = key % 10000;
+      vector<pair<int, int>>& vals = data[hash];
+      for(auto it = vals.begin(); it != vals.end(); ++it)
       {
-        Data_t* prev = data_->prev;
-        Data_t* next = data_->next;
-        
-        if(prev != nullptr)
+        if(it->first == key)
         {
-          prev->next = data_->next;
+          vals.erase(it);
+          break;
         }
-        if(next != nullptr)
-        {
-          next->prev = data_->prev;
-        }
-        
-        if(data_ == data)
-        {
-          data = prev;
-        }
-        
-        data_->next = nullptr;
-        data_->prev = nullptr;
-        delete data_;
-        
-        keys[key] = nullptr;
       }
     }
 };
@@ -136,3 +123,93 @@ public:
  * int param_2 = obj->get(key);
  * obj->remove(key);
  */
+
+//Below code consumes too much memory!!
+// class MyHashMap 
+// {
+// private:
+  
+//   struct Data_t
+//   {
+//     int val;
+//     Data_t* next;
+//     Data_t* prev;
+    
+//     Data_t(int val_) : val(val_), next(nullptr), prev(nullptr) {};
+//   };
+  
+//   Data_t* data;
+//   vector<Data_t*> keys;
+  
+// public:
+//     /** Initialize your data structure here. */
+//     MyHashMap() 
+//       : data(nullptr)
+//     {
+//       keys.resize(1000001, nullptr);
+//     }
+    
+//     /** value will always be non-negative. */
+//     void put(int key, int value) 
+//     {
+//       Data_t* data_ =  new Data_t(value);
+      
+//       if(data == nullptr)
+//       {
+//         data = data_;
+//       }
+//       else
+//       {
+//         data->next = data_;
+//         data_->prev = data;
+//         data = data_;
+//       }
+      
+//       keys[key] = data_;
+//     }
+    
+//     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+//     int get(int key) 
+//     {      
+//       return (keys[key] != nullptr) ? keys[key]->val : -1;
+//     }
+    
+//     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+//     void remove(int key) 
+//     {
+//       Data_t* data_ = keys[key];
+//       if(data_ != nullptr)
+//       {
+//         Data_t* prev = data_->prev;
+//         Data_t* next = data_->next;
+        
+//         if(prev != nullptr)
+//         {
+//           prev->next = data_->next;
+//         }
+//         if(next != nullptr)
+//         {
+//           next->prev = data_->prev;
+//         }
+        
+//         if(data_ == data)
+//         {
+//           data = prev;
+//         }
+        
+//         data_->next = nullptr;
+//         data_->prev = nullptr;
+//         delete data_;
+        
+//         keys[key] = nullptr;
+//       }
+//     }
+// };
+
+// /**
+//  * Your MyHashMap object will be instantiated and called as such:
+//  * MyHashMap* obj = new MyHashMap();
+//  * obj->put(key,value);
+//  * int param_2 = obj->get(key);
+//  * obj->remove(key);
+//  */
