c174f273c8fab62769ab5eb39393db96e9ef735d
batbug <bv.basavaraju@gmail.com>
2021-04-25 21:34:15 +0530

    + daily challenge

---
+++ leetcode/30_days_challenge/2021_4_April/25.cpp
@@ -0,0 +1,83 @@
+/****************************************************
+Date: April 25th
+
+link: https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/596/week-4-april-22nd-april-28th/3717/
+****************************************************/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <limits.h>
+
+using namespace std;
+
+/*
+  Q: Rotate Image
+
+  You are given an n x n 2D matrix representing an image, 
+  rotate the image by 90 degrees (clockwise).
+
+  You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
+  DO NOT allocate another 2D matrix and do the rotation.
+
+  Example 1:
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+  Example 2:
+    Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+
+  Example 3:
+    Input: matrix = [[1]]
+    Output: [[1]]
+
+  Example 4:
+    Input: matrix = [[1,2],[3,4]]
+    Output: [[3,1],[4,2]]
+
+  Constraints:
+    matrix.length == n
+    matrix[i].length == n
+    1 <= n <= 20
+    -1000 <= matrix[i][j] <= 1000
+*/
+
+class Solution 
+{
+public:
+  void rotate(vector<vector<int>>& matrix) 
+  {
+    int row = matrix.size();
+    int col = matrix[0].size();
+    
+    int loop_count = row/2;
+    for(int i = 0; i < loop_count; ++i)
+    {
+      for(int j = i; j < (col-1)-i; ++j)
+      {
+        pair<int, int> p = {i, j};
+
+        //4 times rotation
+        int num = matrix[p.first][p.second];
+        for(int k = 0; k < 4; ++k)
+        {
+          pair<int, int> np = {p.second, (col-1)-p.first};
+
+          int temp = matrix[np.first][np.second];
+          matrix[np.first][np.second] = num;
+          num = temp;
+          p = np;
+        }
+      }
+    }
+  }
+};
