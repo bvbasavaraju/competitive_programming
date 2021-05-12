ade26bf4aa037d9585dfbce9f4bcc3eb8f65b361
batbug <bv.basavaraju@gmail.com>
2021-05-12 07:48:21 +0530

    + daily challenge

---
+++ leetcode/30_days_challenge/2021_5_May/11.cpp
@@ -0,0 +1,103 @@
+/****************************************************
+Date: May 11th
+
+link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3737/
+****************************************************/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <limits.h>
+
+using namespace std;
+/*
+  Q: Maximum Points You Can Obtain from Cards
+
+  There are several cards arranged in a row, and each card has an associated number of points 
+  The points are given in the integer array cardPoints.
+
+  + In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+  + Your score is the sum of the points of the cards you have taken.
+  + Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+
+  Example 1:
+    Input: cardPoints = [1,2,3,4,5,6,1], k = 3
+    Output: 12
+    Explanation: 
+      After the first step, your score will always be 1. However, 
+      choosing the rightmost card first will maximize your total score. 
+      The optimal strategy is to take the three cards on the right, giving a final score of 1 + 6 + 5 = 12.
+
+  Example 2:
+    Input: cardPoints = [2,2,2], k = 2
+    Output: 4
+    Explanation:
+      Regardless of which two cards you take, your score will always be 4.
+
+  Example 3:
+    Input: cardPoints = [9,7,7,9,7,7,9], k = 7
+    Output: 55
+    Explanation:
+      You have to take all the cards. Your score is the sum of points of all cards.
+
+  Example 4:
+    Input: cardPoints = [1,1000,1], k = 1
+    Output: 1
+    Explanation:
+      You cannot take the card in the middle. Your best score is 1.
+
+  Example 5:
+    Input: cardPoints = [1,79,80,1,1,1,200,1], k = 3
+    Output: 202
+
+  Constraints:
+    1 <= cardPoints.length <= 10^5
+    1 <= cardPoints[i] <= 10^4
+    1 <= k <= cardPoints.length
+
+  Hide Hint #1  
+    Let the sum of all points be total_pts. You need to remove a sub-array from cardPoints with length n - k.
+  
+  Hide Hint #2  
+    Keep a window of size n - k over the array. The answer is max(answer, total_pts - sumOfCurrentWindow)
+*/
+
+class Solution 
+{
+public:
+  int maxScore(vector<int>& cardPoints, int k) 
+  {
+    int sum_s = 0;
+    int l = cardPoints.size();
+    for(int i = 0; i < k; i++)
+    {
+      sum_s += cardPoints[i];
+    }
+    
+    if(l == k)
+    {
+      return sum_s;
+    }
+    
+    int sum_e = 0;
+    int ans = sum_s;
+    int pos_s = k - 1;
+    int pos_e = l - 1;
+    for(int i = 0; i < k; ++i)
+    {
+      sum_s -= cardPoints[pos_s--];
+      sum_e += cardPoints[pos_e--];
+      ans = max(ans, sum_s + sum_e);
+    }
+    
+    return ans;
+  }
+};
