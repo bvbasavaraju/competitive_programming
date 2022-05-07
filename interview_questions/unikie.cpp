4f62ac1e396f0f34748f4f49bcacc8e4c512d9e9
batbug <bv.basavaraju@gmail.com>
2022-05-07 15:46:28 +0530

    + unikie test

---
+++ interview_questions/unikie.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+  Q:  Correctness of binary search
+*/
+
+/*
+  Q: finding the amplitude of temperatures in 4 seasons
+*/
+// Here I missed one edge condition, when there are only 1 entry in the temperatures!!
+
+/*
+  Q: Calculating the test score!
+*/
