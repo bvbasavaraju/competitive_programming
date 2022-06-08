7cdecd1409174c17c0787beceae5104ecfecbc5e
batbug <bv.basavaraju@gmail.com>
2022-06-08 21:20:03 +0530

    +practice

--- interview_bit/array/max_min.cpp
+++ interview_bit/array/max_min.cpp
@@ -1,16 +1,35 @@
-#include<bits/stdc++.h>
-using namespace std;
+pair<int, int> getMaxMin(vector<int>& A, int start, int end)
+{
+    if(start == end)
+    {
+        return {A[start], A[start]};
+    }
+
+    int mid = start + (end-start)/2;
+    pair<int, int> p1 = getMaxMin(A, start, mid);
+    pair<int, int> p2 = getMaxMin(A, mid+1, end);
+
+    int mini = min(p1.first, p2.first);
+    int maxi = max(p1.second, p2.second);
+
+    return {mini, maxi};
+}
 
 int Solution::solve(vector<int> &A) 
 {
-    int mini = INT_MAX;
-    int maxi = INT_MIN;
+    // int mini = INT_MAX;
+    // int maxi = INT_MIN;
 
-    for(int a : A)
-    {
-        mini = min(mini, a);
-        maxi = max(maxi, a);
-    }
+    // for(int a : A)
+    // {
+    //     mini = min(mini, a);
+    //     maxi = max(maxi, a);
+    // }
 
-    return maxi + mini;
+    // return maxi + mini;
+
+    //much faster implementation!
+    pair<int, int> minMaxPair = getMaxMin(A, 0, A.size()-1);
+
+    return minMaxPair.second + minMaxPair.first;
 }
