3035abba0de33be09a5f6b936de7b196762f025a
batbug <bv.basavaraju@gmail.com>
2022-06-12 22:02:00 +0530

    + practice

--- practice/44.cpp
+++ practice/44.cpp
@@ -1,43 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//Getting time limit exceeded!! - need to optimize!!
 class Solution 
 {
-private:
-  bool doesMatch(const string& str, const string& pattern, int sp, int pp)
-  {
-    if((sp < 0) && (pp < 0))
-    {
-      return true;
-    }
-    
-    if(sp < 0 && (pp >= 0 && pattern[pp] == '*'))
-    {
-      return doesMatch(str, pattern, sp, pp-1);
-    }
-        
-    if(sp < 0 || pp < 0)
-    {
-      return false;
-    }
-    
-    if((str[sp] == pattern[pp]) || (pattern[pp] == '?'))
-    {
-      return doesMatch(str, pattern, sp-1, pp-1);
-    }
-    else if(pattern[pp] == '*')
-    {
-//       bool skipPattern = doesMatch(str, pattern, sp, pp-1);
-//       bool skipCharacter = doesMatch(str, pattern, sp-1, pp);
-      
-//       return skipPattern || skipCharacter;
-      return (doesMatch(str, pattern, sp, pp-1) || doesMatch(str, pattern, sp-1, pp));
-    }
-    
-    return false;
-  }
-  
+private:  
   bool doesMatch(const string& str, const string& pattern, int sp, int pp, vector<vector<int>>& dp)
   {
     if((sp < 0) && (pp < 0))
@@ -47,12 +13,12 @@ private:
     
     if(sp >= 0 && pp >= 0 && dp[sp][pp] != -1)
     {
-      return dp[sp][pp];
+      return dp[sp][pp] == 1;
     }
     
     if(sp < 0 && (pp >= 0 && pattern[pp] == '*'))
     {
-      return doesMatch(str, pattern, sp, pp-1);
+      return doesMatch(str, pattern, sp, pp-1, dp);
     }
         
     if(sp < 0 || pp < 0)
@@ -62,12 +28,12 @@ private:
     
     if((str[sp] == pattern[pp]) || (pattern[pp] == '?'))
     {
-      dp[sp][pp] = doesMatch(str, pattern, sp-1, pp-1) == true ? 1 : 0 ;
+      dp[sp][pp] = doesMatch(str, pattern, sp-1, pp-1, dp) == true ? 1 : 0 ;
     }
     else if(pattern[pp] == '*')
     {
-      bool skipPattern = doesMatch(str, pattern, sp, pp-1);
-      bool skipCharacter = doesMatch(str, pattern, sp-1, pp);
+      bool skipPattern = doesMatch(str, pattern, sp, pp-1, dp);
+      bool skipCharacter = doesMatch(str, pattern, sp-1, pp, dp);
       
       dp[sp][pp] = (skipPattern || skipCharacter) ? 1 : 0;
     }
@@ -78,11 +44,53 @@ private:
 public:
   bool isMatch(string s, string p) 
   {
-    // return doesMatch(s, p, s.size()-1, p.size()-1);
-    
     int sl = s.size();
     int pl = p.size();
     vector<vector<int>> dp(sl, vector<int>(pl, -1));
     return doesMatch(s, p, s.size()-1, p.size()-1, dp);
+    
+//     int sl = s.size();
+//     int pl = p.size();
+    
+//     vector<vector<bool>> dp(sl+1, vector<bool>(pl+1, false));
+    
+//     for(int i = 0; i <= sl; ++i)
+//     {
+//       for(int j = 0; j <= pl; ++j)
+//       {
+//         if(i == 0 && j == 0)
+//         {
+//           dp[i][j] = true;
+//           continue;
+//         }
+        
+//         if(i == 0)
+//         {
+//           dp[i][j] = p[j-1] == '*' ? dp[i][j-1] : false;
+//           continue;
+//         }
+        
+//         if(j == 0)
+//         {
+//           dp[i][j] = false;
+//           continue;
+//         }
+        
+//         if(p[j-1] == '?')
+//         {
+//           dp[i][j] = dp[i-1][j-1];
+//         }
+//         else if(p[j-1] == '*')
+//         {
+//           dp[i][j] = (dp[i-1][j] || dp[i][j-1]);
+//         }
+//         else 
+//         {
+//           dp[i][j] = (s[i-1] == p[j-1]) ? dp[i-1][j-1] : false;
+//         }
+//       }
+//     }
+    
+//     return dp[sl][pl];
   }
 };
