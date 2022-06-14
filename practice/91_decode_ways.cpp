871efe20cb9d5c7802026dda6d127eb1cac0ce07
batbug <bv.basavaraju@gmail.com>
2022-06-15 00:23:19 +0530

    + practice

--- practice/91_decode_ways.cpp
+++ practice/91_decode_ways.cpp
@@ -1,90 +1,41 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 class Solution 
-{
-private:
-  
-//   void getStrings(string s, string ds, unordered_map<string, string>& kv, vector<string>& strs)
-//   {
-//     if(s.empty())
-//     {
-//       if(!ds.empty())
-//       {
-//         strs.push_back(ds);
-//       }
-      
-//       return;
-//     }
-    
-//     //int l = s.size();
-//     string s1 = s.substr(0, 1);
-//     if(kv.count(s1) != 0)
-//     {
-//       getStrings(s.substr(1), ds+kv[s1], kv, strs);
-//     }
-
-//     if(s.size() > 1)
-//     {
-//       string s2 = s.substr(0, 2);
-//       if(kv.count(s2) > 0)
-//       {
-//         getStrings(s.substr(2), ds+kv[s2], kv, strs); 
-//       }
-//     }
-//   }
-  
-  void getCount(string s, int pos, int& ans)
-  {
-    if(pos >= s.size())
-    {
-      ans += 1;
-      return;
-    }
-    
-    int n = s[pos] - '0';
-    
-    if(n > 0)
-    {
-      bool callForNextSingleDigit = true;
-      if(pos < s.size()-1)
-      {
-        int next = (s[pos+1] - '0');
-        callForNextSingleDigit = (next != 0);
-        n = (n * 10) + next;  
-        if(n < 27)
-        {
-          getCount(s, pos+2, ans);
-        }
-      }  
-      
-      if(callForNextSingleDigit)
-      {
-        getCount(s, pos+1, ans); 
-      }
-    }
-  }
-  
+{  
 public:
   int numDecodings(string s) 
   {
-    int ans = 0;
-    getCount(s, 0, ans);
+    if(s[0] == '0')
+    {
+      return 0;
+    }
     
-    return ans;
-//     int l = s.size();
+    int l = s.size();
+    vector<int> countAtPos(l+1, 0);
+    countAtPos[0] = 1;
+    countAtPos[1] = 1;
     
-//     unordered_map<string, string> keyValues;
-//     for(int i = 0; i < 26; ++i)
-//     {
-//       keyValues[std::to_string(i+1)] = string(1, 'A'+i);
-//     }
+    for(int i = 1; i < l; ++i)
+    {
+      int count = 0;
+      if(s[i] > '0')
+      {
+        count = countAtPos[i];
+      }
+      
+      if(s[i-1] == '1')
+      {
+        count += countAtPos[i-1];
+      }
+      else if(s[i-1] == '2' && s[i] <= '6')
+      {
+        count += countAtPos[i-1];
+      }
+      
+      countAtPos[i+1] = count;
+    }
     
-//     string ds = "";
-//     vector<string> strs;
-//     getStrings(s, ds, keyValues, strs);
-    
-//     return strs.size();
+    return countAtPos[l];
   }
 };
